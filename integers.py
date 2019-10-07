4058729c02e0e1abc47f4ff45c66d3feb4c6d5cd
Trinity-Armstrong <Trinity.Armstrong@mths.ca>
2019-10-07 04:46:34 +0000

    initial commit

--- integers.py
+++ integers.py
@@ -9,15 +9,16 @@ def main():
     # This function tells you if a number is positive, negative, or equal to 0
 
     # input
-    user_input = int(input("Enter an integer: ")
+    user_input = int(input("Enter an integer: "))
 
     # process
-    if user_input > 0
+    if user_input > 0:
         print("This is a positive integer. ")
-    elif user_input < 0
+    elif user_input < 0:
         print("This is a negative integer. ")
-    elif user_input == 0
+    elif user_input == 0:
         print("This integer is zero. ")
+
 
 if __name__ == "__main__":
     main()
